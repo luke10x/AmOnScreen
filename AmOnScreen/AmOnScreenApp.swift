96e19435bafc73222d429534391deccb87c53d6a
Luke 10X <spam@luke10x.dev>
2023-02-16 20:42:07 -0500

    🇱🇹 Independece day

--- AmOnScreen/AmOnScreenApp.swift
+++ AmOnScreen/AmOnScreenApp.swift
@@ -6,12 +6,60 @@
 //
 
 import SwiftUI
+import Network
 
 @main
 struct AmOnScreenApp: App {
+    let echoServer = EchoServer()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(echoServer: echoServer)
         }
+    }
+}
+
+class EchoServer {
+    var listener: NWListener?
+    var shouldEchoProperty: Bool = false
+    
+    init() {
+        start()
+    }
+    
+    func start() {
+        do {
+            listener = try NWListener(using: .tcp, on: 12345)
+            listener?.stateUpdateHandler = { newState in
+                switch newState {
+                case .ready:
+                    print("Listener started")
+                case .failed(let error):
+                    print("Listener failed with error: \(error)")
+                default:
+                    break
+                }
+            }
+            listener?.newConnectionHandler = { [weak self] newConnection in
+                print("New connection accepted")
+                newConnection.start(queue: .global(qos: .userInitiated))
+                self?.handleConnection(newConnection)
+            }
+            listener?.start(queue: .global(qos: .userInitiated))
+        } catch {
+            print("Failed to start listener with error: \(error)")
+        }
+    }
+    
+    func handleConnection(_ connection: NWConnection) {
+        let response = shouldEchoProperty ? "OK" : "NO"
+        let responseData = Data(response.utf8)
+        connection.send(content: responseData, completion: .contentProcessed({ error in
+            if let error = error {
+                print("Failed to send response with error: \(error)")
+                return
+            }
+            connection.cancel()
+        }))
     }
 }
--- AmOnScreen/ContentView.swift
+++ AmOnScreen/ContentView.swift
@@ -1,26 +1,61 @@
-//
-//  ContentView.swift
-//  AmOnScreen
-//
-//  Created by Luke English on 2/16/23.
-//
-
 import SwiftUI
+import UserNotifications
+import Foundation
+import CoreFoundation
 
 struct ContentView: View {
+
+    let echoServer: EchoServer
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "recordingtape")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text("Recording")
         }
         .padding()
+        
+        .onAppear {
+
+            var count = 0
+            _ = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                
+                let app = NSApplication.shared
+                let windows = app.windows
+                
+                let window = windows.first
+                window?.level = .popUpMenu
+                
+                count = count + 1
+                
+                if window?.isOnActiveSpace == true {
+                    print("window is here " + String(count))
+                    echoServer.shouldEchoProperty = true
+                    
+                } else {
+                    print("window is not here " + String(count))
+                    echoServer.shouldEchoProperty = false
+                }
+                
+            }
+            
+            echoServer.start()
+        }
+        .onDisappear {
+            print("stopping")
+            echoServer.listener?.cancel()
+            NSApplication.shared.terminate(self)
+        }
     }
 }
 
+let echoServer = EchoServer()
+
 struct ContentView_Previews: PreviewProvider {
+
     static var previews: some View {
-        ContentView()
+        ContentView(echoServer: echoServer)
     }
 }
