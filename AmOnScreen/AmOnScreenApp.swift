9607a5907fa83187c1b945617e1d6fd422071621
Luke 10X <spam@luke10x.dev>
2023-02-16 20:32:25 -0500

    Initial Commit

---
+++ AmOnScreen/AmOnScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmOnScreenApp.swift
+//  AmOnScreen
+//
+//  Created by Luke English on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AmOnScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
